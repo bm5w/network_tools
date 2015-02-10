e0a8637f26771eaa45c96fafa0b6bed772e1cef4
Mark Saiget <saiget@gmail.com>
2015-02-10 10:27:31 -0800

    Add keyboard interrupt for server.

--- echo_server.py
+++ echo_server.py
@@ -10,14 +10,19 @@ if __name__ == '__main__':
     server_socket.bind(('127.0.0.1', 50000))
     server_socket.listen(1)
     buffsize = 32
-    while True:
-        msg = ''
-        done = False
-        conn, addr = server_socket.accept()
-        while not done:
-            msg_part = conn.recv(buffsize)
-            msg += msg_part
-            if len(msg_part) < buffsize:
-                done = True
-                conn.sendall(msg)
-                conn.close()
+    try:
+        while True:
+            msg = ''
+            done = False
+            conn, addr = server_socket.accept()
+            while not done:
+                msg_part = conn.recv(buffsize)
+                msg += msg_part
+                if len(msg_part) < buffsize:
+                    done = True
+                    conn.sendall(msg)
+                    conn.shutdown(socket.SHUT_WR)
+                    conn.close()
+    except KeyboardInterrupt:
+        print 'I successfully stopped.'
+        server_socket.close()
