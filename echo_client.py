44f0a8f96b95183c2e66bed3c1eb476b746f3380
Mark Saiget <saiget@gmail.com>
2015-02-13 09:04:28 -0800

    Change port to 10000, to see if it works with Travis.

--- echo_client.py
+++ echo_client.py
@@ -6,7 +6,7 @@ import socket
 def client(inputted):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM,
                                   socket.IPPROTO_IP)
-    client_socket.connect(('127.0.0.1', 50000))
+    client_socket.connect(('127.0.0.1', 10000))
     client_socket.sendall(inputted)
     client_socket.shutdown(socket.SHUT_WR)
     buffsize = 4096
